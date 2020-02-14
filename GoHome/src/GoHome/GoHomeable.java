7dfe0e2a447f5eee1f43cd9eefbf74be5237ba37
gleizy <ray.lei.gao@gmail.com>
2020-02-14 16:05:17 +0900

    Add files via upload

---
+++ GoHome/src/GoHome/GoHomeable.java
@@ -0,0 +1,23 @@
+package GoHome;
+
+public interface GoHomeable {
+	/**
+	 * 退社
+	 */
+	public void leaveOffice();
+	
+	/**
+	 * 帰宅
+	 */
+	public void goHome();
+	
+	/**
+	 * 晩御飯
+	 */
+	public void dinner();
+	
+	/**
+	 * 寝る
+	 */
+	public void sleep();
+}
---
+++ GoHome/src/GoHome/Main.java
@@ -0,0 +1,17 @@
+package GoHome;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static void doGoHome(GoHomeable go) {
+		go.leaveOffice();
+		go.goHome();
+		go.dinner();
+		go.sleep();
+	}
+
+}
